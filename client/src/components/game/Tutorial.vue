a08eb51620a611422cbab024ec057f6894383958
remi <remi.rucojevic@gmail.com>
2021-06-19 15:02:47 +0200

    img

--- client/src/components/game/Tutorial.vue
+++ client/src/components/game/Tutorial.vue
@@ -1,8 +1,77 @@
 <template>
   <div class="tutorial step">
-    <StepTitle>Tutorial</StepTitle>
-    <StarBtn v-on:click="readyForNext" :valid="$store.state.player?.isReady"
-      >Suivant</StarBtn
+    <img src="/img/titles/depart.svg" />
+    <MinimalistArrowBtn
+      v-if="step > 1"
+      class="btn-left"
+      direction="left"
+      @click="changeStep('-')"
+      >Précédent</MinimalistArrowBtn
+    >
+    <MinimalistArrowBtn
+      v-if="step < 4"
+      class="btn-right"
+      direction="right"
+      @click="changeStep('+')"
+      >Suivant</MinimalistArrowBtn
+    >
+    <div class="tuto-container">
+      <div
+        class="tuto-step"
+        :style="{ transform: 'translateX(-' + translate + '%)' }"
+      >
+        <img src="/img/animations/tuto1.gif" />
+        <p>
+          C’est le début des vacances !<br />Vous allez vous affronter dans des
+          minis jeux.
+        </p>
+      </div>
+      <div
+        class="tuto-step"
+        :style="{ transform: 'translateX(calc(-' + translate + '% - 32px))' }"
+      >
+        <img src="/img/animations/tuto2.gif" />
+        <p>Le petit jaune</p>
+        <p>
+          C’est l’heure de l’apéro ! Profitez-en pour prendre de l’avance !
+          Chargez un peu le verre des autres pour réduire leur visibilité.
+        </p>
+      </div>
+      <div
+        class="tuto-step"
+        :style="{ transform: 'translateX(calc(-' + translate + '% - 64px))' }"
+      >
+        <img src="/img/animations/tuto3.gif" />
+        <p>Le coup de pouce</p>
+        <p>
+          Votre GPS ne marche plus et vous êtes un peu perdu ? Le coup de pouce
+          vous vient en aide !
+        </p>
+      </div>
+      <div
+        class="tuto-step"
+        :style="{ transform: 'translateX(calc(-' + translate + '% - 96px))' }"
+      >
+        <img src="/img/animations/tuto4.gif" />
+        <p>Le champi</p>
+        <p>
+          Vous avez déjà répondu ? Appuyez sur le champi pour déconcentrer les
+          autres joueurs !
+        </p>
+      </div>
+    </div>
+    <div class="points">
+      <div class="point" :class="{ active: step == 1 }"></div>
+      <div class="point" :class="{ active: step == 2 }"></div>
+      <div class="point" :class="{ active: step == 3 }"></div>
+      <div class="point" :class="{ active: step == 4 }"></div>
+    </div>
+    <StarBtn
+      class="btn"
+      v-on:click="readyForNext"
+      :valid="$store.state.player?.isReady"
+      :big="true"
+      >C'est<br />parti !</StarBtn
     >
   </div>
 </template>
@@ -16,6 +85,27 @@ export default class Tutorial extends Vue {
   $refs!: any;
   $store!: Store<StoreState>;
 
+  step = 1;
+  maxStep = 4;
+
+  get translate() {
+    return 100 * (this.step - 1);
+  }
+
+  changeStep(direction: string) {
+    if (direction === "-") {
+      this.step--;
+    } else if (direction === "+") {
+      this.step++;
+    }
+
+    if (this.step > this.maxStep) {
+      this.step = 1;
+    } else if (this.step < 1) {
+      this.step = this.maxStep;
+    }
+  }
+
   readyForNext(): void {
     this.$store.state.sounds.cta.howl.play();
     this.$store.dispatch("readyForNext");
@@ -24,4 +114,63 @@ export default class Tutorial extends Vue {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.tuto-container {
+  display: flex;
+  .tuto-step {
+    min-width: calc(100vw - 64px);
+    margin-right: 32px;
+
+    font-family: $btnFont;
+    font-weight: normal;
+    font-size: 1.8rem;
+
+    transition: transform 0.3s;
+  }
+}
+
+.points {
+  display: flex;
+  justify-content: center;
+
+  .point {
+    width: 8px;
+    height: 8px;
+
+    opacity: 0.25;
+
+    margin-right: 8px;
+    border-radius: 50%;
+
+    transition: opacity 0.3s;
+
+    background-color: black;
+
+    &.active {
+      opacity: 1;
+    }
+  }
+}
+
+.btn-left {
+  position: absolute;
+  top: 50%;
+  left: 0;
+  z-index: 20;
+  cursor: pointer;
+}
+
+.btn-right {
+  position: absolute;
+  top: 50%;
+  right: 0;
+  z-index: 20;
+  cursor: pointer;
+}
+
+.btn {
+  position: absolute;
+  bottom: 0;
+  right: 0;
+}
+</style>
