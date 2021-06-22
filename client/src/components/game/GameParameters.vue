50762d352e935efce1ddbf20b9bcacecd987583e
Maël Atzori Maltete <mael@gotoandbuzz.com>
2021-06-22 18:25:47 +0200

    Upd

--- client/src/components/game/GameParameters.vue
+++ client/src/components/game/GameParameters.vue
@@ -24,6 +24,7 @@
       </div>
 
       <div class="parameters">
+        <img class="annotation-2" src="/img/annotations/mode_de_jeu.svg" />
         <div
           class="parameters-container"
           :class="{ canInteract: this.$store.state.player.isMDR }"
@@ -181,6 +182,11 @@ export default class GameParameters extends Vue {
   position: absolute;
   top: -68%;
   right: -60%;
+}
+.annotation-2 {
+  position: absolute;
+  right: 230px;
+  top: 230px;
 }
 .gameParameters-content {
   display: flex;
